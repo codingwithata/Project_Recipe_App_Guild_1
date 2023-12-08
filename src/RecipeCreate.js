74d94fcdc655140a2dce43cc1b73ebd48e87d65b
Malik <Mata@indeed.com>
2023-12-08 06:42:26 -0800

    Update recipe list to display content

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,132 +1,99 @@
 import React, { useState } from "react";
-import "./RecipeCreate.css"
+import "./RecipeCreate.css";
 
-function RecipeCreate() {
-
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers
-  
+function RecipeCreate({ setRecipes }) {
   const initialFormState = {
     name: "",
     cuisine: "",
     photo: "",
     ingredients: "",
-    preparation: ""
+    preparation: "",
   };
 
   const [formData, setFormData] = useState({ ...initialFormState });
 
-  // const [name, setName] = useState("");
-  // const [cuisine, setCuisine] = useState("");
-  // const [photo, setPhoto] = useState("");
-  // const [rating, setRating] = useState("");
-  // const [ingredients, setIngredients] = useState("");
-  // const [preparation, setPreparation] = useState("");
-  
   const handleChange = ({ target }) => {
     const { name, value } = target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
-  
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(formData)
-    setFormData({
-      ...initialFormState
-    })
-  }
+    event.preventDefault();
+    setRecipes((prevRecipes) => [...prevRecipes, formData]);
+    setFormData({ ...initialFormState });
+  };
 
   return (
     <form onSubmit={handleSubmit} name="create">
-  <table>
-  <tbody>
-  <tr>
-    <th>Name</th>
-    <th>Cuisine</th>
-    <th>Photo</th>
-    <th>Ingredients</th>
-    <th>Preparation</th>
-    <th>Actions</th>
-  </tr>
-  
-      <tr>
-        <td>
-        <label>
-          <input
-            id="name"
-          type="text"
-          name="name"
-          onChange={handleChange}
-          value={formData.name} 
-          >
-          </input>
-          </label>
-        </td>
-        <td>
-        <label>
-
-          <input
-          id="cuisine"
-          type="text"
-          name="cuisine"
-          onChange={handleChange}
-          value={formData.cuisine} 
-          >
-          </input>
-          </label>
-        </td>
-        <td>
-        <label>
-          <input
-            id="photo"
-          type="text"
-          name="photo"
-          onChange={handleChange}
-          value={formData.photo} 
-          >
-          </input>
-          </label>
-        </td>
-        <td>
-        <label>
-          <textarea
-          id="ingredients"
-          type="text"
-          name="ingredients"
-          onChange={handleChange}
-          value={formData.ingredients} 
-          >
-          </textarea>
-          </label>
-        </td>
-        <td>
-        <label>
-          <textarea
-          id="preparation"
-          type="textarea"
-          name="preparation"
-          onChange={handleChange}
-          value={formData.preparation} 
-          >
-          </textarea>
-          </label>
-        </td>
-        <td>
-          <button type="submit">Create</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</form>
-
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <label>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  onChange={handleChange}
+                  value={formData.name}
+                />
+              </label>
+            </td>
+            <td>
+              <label>
+                <input
+                  id="cuisine"
+                  type="text"
+                  name="cuisine"
+                  onChange={handleChange}
+                  value={formData.cuisine}
+                />
+              </label>
+            </td>
+            <td>
+              <label>
+                <input
+                  id="photo"
+                  type="text"
+                  name="photo"
+                  onChange={handleChange}
+                  value={formData.photo}
+                />
+              </label>
+            </td>
+            <td>
+              <label>
+                <textarea
+                  id="ingredients"
+                  type="text"
+                  name="ingredients"
+                  onChange={handleChange}
+                  value={formData.ingredients}
+                />
+              </label>
+            </td>
+            <td>
+              <label>
+                <textarea
+                  id="preparation"
+                  type="textarea"
+                  name="preparation"
+                  onChange={handleChange}
+                  value={formData.preparation}
+                />
+              </label>
+            </td>
+            <td>
+              <button type="submit">Create</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
   );
 }
 
 export default RecipeCreate;
-
-
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,20 +1,43 @@
 import React from "react";
+function RecipeRow({ recipe, onDelete }) {
+  return (
+    <tr key={recipe.id}>
+      <td>{recipe.name}</td>
+      <td>{recipe.cuisine}</td>
+      <td>{recipe.photo}</td>
+      <td>{recipe.ingredients}</td>
+      <td>{recipe.preparation}</td>
+      <td>
+        <button onClick={() => onDelete(recipe.id)} name="delete">
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
 
-function RecipeList() {
-  
-  // TODO: Display the list of recipes using the structure of table that is provided.
-  // TODO: Create at least one additional component that is used by this component.
-  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+function RecipeList({ recipes, setRecipes }) {
+  const handleDelete = (id) => {
+    setRecipes((prevRecipes) => prevRecipes.filter((recipe) => recipe.id !== id));
+  };
 
   return (
     <div className="recipe-list">
       <table>
         <thead>
           <tr>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingredients</th>
+            <th>Preparation</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-        
+          {recipes.map((recipe) => (
+            <RecipeRow key={recipe.id} recipe={recipe} onDelete={handleDelete} />
+          ))}
         </tbody>
       </table>
     </div>
