0d0e031acbbd49b7befcef3d3a67ef568488e6f3
Malik <Mata@indeed.com>
2023-12-08 07:03:52 -0800

    Add css for list component

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,21 +1,6 @@
 import React from "react";
-function RecipeRow({ recipe, onDelete }) {
-  return (
-    <tr key={recipe.id}>
-      <td>{recipe.name}</td>
-      <td>{recipe.cuisine}</td>
-      <td><img src={recipe.photo} alt={`Image`} /> </td>
-      <td>{recipe.ingredients}</td>
-      <td>{recipe.preparation}</td>
-      <td>
-        <button onClick={() => onDelete(recipe.id)} name="delete">
-          Delete
-        </button>
-      </td>
-    </tr>
-  );
-}
-
+import RecipeRow from "./RecipeRow";
+import "./RecipeList.css"
 function RecipeList({ recipes, setRecipes }) {
 
     const handleDelete = (index) => {
